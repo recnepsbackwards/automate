cca39721c41d0a39cdd2f293a90c15fb0c891bd9
Spencer West <spencer.west@kbb.com>
2018-03-14 23:00:41 -0700

    updates

--- content_script.js
+++ content_script.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "testScript.js"});
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript({
+        file: 'jquery-3.3.1.min.js'
+    }, function() {
+        // Guaranteed to execute only after the previous script returns
+        chrome.tabs.executeScript({
+            file: 'testScript.js'
+        });
+    });
 });
--- testScript.js
+++ testScript.js
@@ -1,1 +1,35 @@
-alert('icon clicked');
+$(document).ready(function() {
+  var url = window.location.href;
+  if (url.indexOf('overview') > -1) {
+    alert('overview');
+    $('business-hours').remove();
+    var arr = [];
+    $('md-input-container.md-input-has-value').each(function(e) {
+      var label = $(this).find('label').text();
+      var input = $(this).find('input').val();
+      var text = $(this).find('md-select-value span .md-text').text();
+      if (input) {
+        var inputArray = [];
+        inputArray.push(label);
+        inputArray.push(input);
+        arr.push(inputArray);
+      }
+      else {
+        var inputArray = [];
+        inputArray.push(label);
+        inputArray.push(text);
+        arr.push(inputArray);
+      }
+    });
+    console.log(arr);
+  }
+  else if (url.indexOf('account-success') > -1) {
+    alert('account-successs');
+    $('md-input-container').each(function(e) {
+    	console.log($(this).find('label').text() + " " + $(this).find('input').val());
+    });
+  }
+  else {
+    alert('none');
+  }
+});
